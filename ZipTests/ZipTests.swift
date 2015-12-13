c22dca5291adfe1cb4de21041165db3b0a9fa55a
Roy Marmelstein <marmelroy@gmail.com>
2015-12-13 20:34:47 +0100

    Initial commit

---
+++ ZipTests/ZipTests.swift
@@ -0,0 +1,36 @@
+//
+//  ZipTests.swift
+//  ZipTests
+//
+//  Created by Roy Marmelstein on 13/12/2015.
+//  Copyright © 2015 Roy Marmelstein. All rights reserved.
+//
+
+import XCTest
+@testable import Zip
+
+class ZipTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
