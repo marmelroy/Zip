fd5ed81d6ec6bae41743d77a7c6e025611c15ba0
Roy Marmelstein <marmelroy@gmail.com>
2015-12-13 23:38:52 +0100

    Building a minizip module

---
+++ Zip/Zip.swift
@@ -0,0 +1,15 @@
+//
+//  Zip.swift
+//  Zip
+//
+//  Created by Roy Marmelstein on 13/12/2015.
+//  Copyright © 2015 Roy Marmelstein. All rights reserved.
+//
+
+import Foundation
+import minizip
+
+class Zip {
+    func helloWorld() {
+    }
+}
