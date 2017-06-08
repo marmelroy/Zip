5944652196d2163edfb2df5f371d548c8195bf9e
Dmytro Komar <kmr.dma@gmail.com>
2017-06-08 15:59:13 +0300

    updated to swift 4.0

--- Zip/Zip.swift
+++ Zip/Zip.swift
@@ -165,8 +165,8 @@ public class Zip {
             let fullPath = destination.appendingPathComponent(pathString).path
 
             let creationDate = Date()
-            let directoryAttributes = [FileAttributeKey.creationDate.rawValue : creationDate,
-                                       FileAttributeKey.modificationDate.rawValue : creationDate]
+            let directoryAttributes = [FileAttributeKey.creationDate : creationDate,
+                                       FileAttributeKey.modificationDate : creationDate]
             do {
                 if isDirectory {
                     try fileManager.createDirectory(atPath: fullPath, withIntermediateDirectories: true, attributes: directoryAttributes)
